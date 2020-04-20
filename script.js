b9f2f0d46853b564374136b3684b9291f47ff3f7
michael Zuniga <michaelzunigaa@gmail.com>
2020-04-19 23:03:51 -0700

    added-day-timeblock-rather-month

--- script.js
+++ script.js
@@ -1,20 +1,65 @@
-var monthNames = [ "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December" ];
-var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
 
-var newDate = new Date();
-newDate.setDate(newDate.getDate() + 1);    
-$('#date').text(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    for (let i = 9; i < 18; i++) {
 
-var $plannerEle = $('#calendar-container');
-  // clear existing elements
-  $plannerEle.empty();
-  for (let hour = 9; hour <=17; hour++)  {
-      let hour = -9;
-      
-  }
+        // create a row
+        const row = $(`<div data-time=${i} id='${i}' class="row">`);
 
+        // create a column
+        const col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
 
-  var $timeBlocks = $(".timeblocks");
-  $timeBlocks.addClass('plannerRow');
-  $timeBlocks.attr('hour-index', hour);
+        //create column 2
+        const col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);
+
+        //create column 3
+        const col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
+
+        // append col to row
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
+
+        // last step add rows to container
+        $(".container").append(row);
+
+        getLocalStorage(i);
+    }
+
+    function formatAMPM(hours) {
+        const ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
+    formatAMPM();
+
+    function updateColors() {
+        const currentTime = new Date().getHours();
+        for (const i = 9; i < 18; i++) {
+            console.log(currentTime, $(`#${i}`).data("time"));
+            if ($(`#${i}`).data("time") == currentTime) {
+                $(`#text${i}`).addClass("present");
+            } else if (currentTime < $(`#${i}`).data("time")) {
+                $(`#text${i}`).addClass("future");
+            }
+        }
+    }
+
+    setInterval(function () {
+        updateColors();
+    }, 1000);
+
+    const saveBtn = $('.saveBtn');
+    saveBtn.on('click', function () {
+        let eventId = $(this).attr('id');
+        let eventText = $(this).parent().siblings().children('.description').val();
+        localStorage.setItem(eventId, eventText);
+    });
+});
